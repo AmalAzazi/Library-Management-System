c468c1f72ea76198544f559256becdb221cf1a40
AmalAzazi <36468144+AmalAzazi@users.noreply.github.com>
2018-03-31 12:16:57 +0800

    Add files via upload

---
+++ HeadLibrarians.java
@@ -0,0 +1,84 @@
+package librarymanagement;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AVRO
+ */
+
+ public class HeadLibrarians{
+   public static void viewlibrarian(){
+       try {
+           BufferedReader op = new BufferedReader(new FileReader("librarian.txt"));
+           System.out.println("Librarian's details");
+           String line=null;
+           while((line=op.readLine())!=null){
+               System.out.println(line);
+            }
+       }catch(Exception e){
+            System.out.println("Error reading file.");
+        }
+   }
+
+public void viewmembers(){
+ boolean viewmembers=false;
+ Scanner input=new Scanner(System.in);
+    System.out.println("Details");
+ try{
+     
+     BufferedReader op=new BufferedReader(new FileReader("member.txt"));
+     System.out.println("Member's detalis");
+     String line=null;
+     while((line=op.readLine())!=null){
+         System.out.println(line);
+     }
+     viewmembers=true;
+     }catch(Exception e){
+ }
+}
+}
+
+
+ 
+       
+ 
+     
+       
+     
+         
+     
+
+     
+             
+                     
+                     
+
+
+           
+        
+                 
+        
+            
+        
+                
+            
+        
+          
+          
+        
+               
+            
+        
+        
+    
+            
+    
+
